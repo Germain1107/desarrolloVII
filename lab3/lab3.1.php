b639228e073944d3ed214234d4c94c603f34bf16
Germain1107 <germainstephens07@gmail.com>
2022-09-21 19:00:17 -0500

    Lab3

--- lab3/lab3.1.php
+++ lab3/lab3.1.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+    $diametro = $_POST['diam'];
+    $altura = $_POST['altu'];
+    $radio = $diametro/2;
+    $pi = 3.141593;
+    $volumen = $pi *$radio*$radio*$altura;
+
+    echo "<br> El volumen del cilindro es de: ".$volumen." metros cubicos";
+?>
